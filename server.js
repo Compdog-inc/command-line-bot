5d16849b32995bdaed6e07c550acb6b9945b17e5
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2021-07-28 10:38:08 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+process.on('uncaughtException', function(error) {
+    console.log(error);
+});
+
+process.on('unhandledRejection', function(reason, p) {
+    console.log(reason);
+});
+
+client.on('ready', function() {
+    console.log("Ready");
+});
+
+client.on('message', function(message) {
+    if(!message.mentions.has(client.user))
+      return;
+  
+    if (message.author == client.user)
+        return;
+
+    if (message.author.bot)
+        return;
+});
+
+client.login(process.env.TOKEN);
